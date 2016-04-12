2cad070b5722ca18fdc2f2c461e00a61499c07db
palmithor <palmithor@gmail.com>
2016-04-12 13:33:18 +0200

    Fix naming upper lower case mixup.

--- app/src/main/java/com/group9/eda397/data/api/GithubService.java
+++
@@ -1,8 +0,0 @@
-package com.group9.eda397.data.api;
-
-/**
- * @author palmithor
- * @since 24/03/16.
- */
-public class GithubService {
-}
--- app/src/main/java/com/group9/eda397/data/github/GitHubService2.java
+++ app/src/main/java/com/group9/eda397/data/github/GitHubService2.java
@@ -14,7 +14,7 @@ import retrofit2.http.Path;
  * @author palmithor
  * @since 24/03/16.
  */
-public interface GitHubService {
+public interface GitHubService2 {
 
     String PATH_PARAM_OWNER = "owner";
     String PATH_PARAM_REPO = "repo";
