baeb8b28a22c5ab71f9fdf4995aadafa4595a8ed
palmithor <palmithor@gmail.com>
2016-04-19 17:38:57 +0200

    Add toolbar and style to TravisBuildDetails.

--- app/src/main/java/com/group9/eda397/ui/activities/TravisBuildDetailsActivity.java
+++ app/src/main/java/com/group9/eda397/ui/activities/TravisBuildDetailsActivity.java
@@ -10,6 +10,7 @@ import com.group9.eda397.R;
 import com.group9.eda397.data.TravisServiceFactory;
 import com.group9.eda397.data.github.TravisService;
 import com.group9.eda397.model.TravisBuildDetails;
+import com.group9.eda397.utils.StringUtils;
 
 import butterknife.Bind;
 import icepick.State;
@@ -24,7 +25,7 @@ import timber.log.Timber;
  * @author palmithor
  * @since 19/04/16.
  */
-public class TravisBuildDetailsActivity extends BaseActivity {
+public class TravisBuildDetailsActivity extends ToolbarActivity {
 
     private static final String EXTRA_BUILD_ID = "travis_build_id_extra";
     private static final String EXTRA_REPOSITORY = "travis_build_owner_extra";
@@ -33,8 +34,8 @@ public class TravisBuildDetailsActivity extends BaseActivity {
     @State protected String repository;
     @State protected String owner;
     @State protected TravisBuildDetails travisBuildDetails;
-    @Bind(R.id.tv_author_name) TextView authorTextView; //TODO Doni uncomment when you've created the layout
-    @Bind(R.id.tv_author_email) TextView authorEmailTextView; //TODO Doni uncomment when you've created the layout
+    @Bind(R.id.tv_author_name) TextView authorTextView;
+    @Bind(R.id.tv_author_email) TextView authorEmailTextView;
     private TravisService travisService;
 
     public static Intent getStartingIntent(final Activity startingActivity, final String owner, final String repository, final Long buildId) {
@@ -67,6 +68,13 @@ public class TravisBuildDetailsActivity extends BaseActivity {
         this.buildId = params.getLong(EXTRA_BUILD_ID);
         this.owner = params.getString(EXTRA_OWNER);
         this.repository = params.getString(EXTRA_REPOSITORY);
+    }
+
+    @Override
+    protected void setupToolbar() {
+        super.setupToolbar();
+        String title = getResources().getString(R.string.title_build) + StringUtils.SPACE + StringUtils.HASHTAG + buildId;
+        toolbar.setTitle(title);
     }
 
     private void fetchBuild() {
