6bacaf727ef8d3e95e69321e5751aee23854c63b
Linus Hagvall <hagvall@student.chalmers.se>
2016-04-19 15:19:37 +0200

    develop merge to timer

--- app/src/main/java/com/group9/eda397/ui/TimerFragment.java
+++ app/src/main/java/com/group9/eda397/ui/TimerFragment.java
@@ -3,12 +3,17 @@ package com.group9.eda397.ui;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.group9.eda397.R;
+import com.group9.eda397.ui.fragments.BaseFragment;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -17,7 +22,7 @@ import butterknife.ButterKnife;
  * @author palmithor
  * @since 14/04/16.
  */
-public class TimerFragment extends Fragment {
+public class TimerFragment extends BaseFragment {
 
     private static final String ARG_TEXT = TimerFragment.class.getCanonicalName() + ".arg_text";
     @Bind(R.id.text) TextView textView;
@@ -36,6 +41,23 @@ public class TimerFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_test, container, false);
         ButterKnife.bind(this, view);
         textView.setText(getArguments().getString(ARG_TEXT));
+        testTimer();
         return view;
+    }
+
+    private void testTimer(){
+        final Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                    textView.setText("Timer");
+            }
+        }, 200);
+
+
+    }
+
+    protected int getLayout() {
+        return R.layout.fragment_test;
     }
 }
