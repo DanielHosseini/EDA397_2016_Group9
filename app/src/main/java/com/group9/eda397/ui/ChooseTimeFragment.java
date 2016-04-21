928238dca47bf195513678fc385436703c5c8ff5
Linus Hagvall <hagvall@student.chalmers.se>
2016-04-21 14:45:40 +0200

    countdownChooser edittext+startButton

--- app/src/main/java/com/group9/eda397/ui/ChooseTimeFragment.java
+++ app/src/main/java/com/group9/eda397/ui/ChooseTimeFragment.java
@@ -5,10 +5,15 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.NumberPicker;
 import android.widget.TextView;
 
 import com.group9.eda397.R;
 import com.group9.eda397.ui.fragments.BaseFragment;
+
+import org.w3c.dom.Text;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -18,14 +23,28 @@ import butterknife.ButterKnife;
  */
 public class ChooseTimeFragment extends BaseFragment {
 
-    private static final String ARG_TEXT = TimerFragment.class.getCanonicalName() + ".arg_text";
-    @Bind(R.id.text) TextView textView;
+    private static final String ARG_TEXT = ChooseTimeFragment.class.getCanonicalName() + ".arg_text";
+    @Bind(R.id.editText) EditText editText;
+    @Bind(R.id.startButton) Button startButton;
 
     @Nullable
     @Override
     public View onCreateView(final LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable final Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.countdown_chooser, container, false);
         ButterKnife.bind(this, view);
+        editText.setText("00");
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setText("");
+            }
+        });
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startButton.setText(editText.getText());
+            }
+        });
         return view;
     }
 
@@ -38,7 +57,7 @@ public class ChooseTimeFragment extends BaseFragment {
     }
 
     public int getLayout(){
-        return R.layout.countdown_timer;
+        return R.layout.countdown_chooser;
     }
 
 }
