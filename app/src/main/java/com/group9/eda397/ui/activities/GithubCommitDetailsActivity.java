d500be06bd735cfc3357492d54b719ceffbbbd4b
palmithor <palmithor@gmail.com>
2016-05-12 13:08:33 +0200

    Add toolbar to github commit details.

--- app/src/main/java/com/group9/eda397/ui/activities/GithubCommitDetailsActivity.java
+++ app/src/main/java/com/group9/eda397/ui/activities/GithubCommitDetailsActivity.java
@@ -1,29 +1,13 @@
 package com.group9.eda397.ui.activities;
 
 import android.app.Activity;
-import android.app.Application;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.view.View;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.group9.eda397.R;
 
-import com.group9.eda397.data.github.GitHubService;
-import com.group9.eda397.model.GitHubCommitItem;
-import com.group9.eda397.utils.StringUtils;
-
-import butterknife.Bind;
-import icepick.State;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import timber.log.Timber;
-public class GithubCommitDetailsActivity  extends Activity {
+public class GithubCommitDetailsActivity extends ToolbarActivity {
 
     // Setting up variables
     private static final String EXTRA_OWNER = "github_commit_owner_extra";
@@ -31,17 +15,9 @@ public class GithubCommitDetailsActivity  extends Activity {
     private static final String EXTRA_COMMIT = "github_commit";
     private static final String EXTRA_SHA = "github_committ_sha";
 
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_github_commit_details);
-        showInfo();
-    }
-
     public static Intent getStartingIntent(final Activity startingActivity,
                                            final String owner, final String email,
-                                           final String commit,final String sha){
+                                           final String commit, final String sha) {
         Intent intent = new Intent(startingActivity, GithubCommitDetailsActivity.class);
         intent.putExtra(EXTRA_OWNER, owner);
         intent.putExtra(EXTRA_EMAIL, email);
@@ -50,9 +26,24 @@ public class GithubCommitDetailsActivity  extends Activity {
         return intent;
     }
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        showInfo();
+    }
 
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_github_commit_details;
+    }
 
-    private void showInfo(){
+    @Override
+    protected void setupToolbar() {
+        super.setupToolbar();
+        toolbar.setTitle(R.string.title_github_commit_details);
+    }
+
+    private void showInfo() {
         Intent intent = getIntent();
         TextView name = (TextView) findViewById(R.id.github_author_name);
         name.setText(intent.getStringExtra(EXTRA_OWNER));
